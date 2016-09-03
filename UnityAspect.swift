ced335d4136700a8494abdebbed432ce72b9f114
Tony Edwards <tnydwrds@gmail.com>
2016-09-02 17:37:05 -0700

    Fix redundant access control

--- UnityAspect.swift
+++ UnityAspect.swift
@@ -6,7 +6,7 @@
 @AttributeUsage(AttributeTargets.Property)
 public class PublicizeField: System.Attribute, IPropertyImplementationDecorator {
 
-    public public func HandleImplementation(_ Services: IServices!, _ aProperty: IPropertyDefinition!, _ aRead: IMethodDefinition!, _ aWrite: IMethodDefinition!) {
+    public func HandleImplementation(_ Services: IServices!, _ aProperty: IPropertyDefinition!, _ aRead: IMethodDefinition!, _ aWrite: IMethodDefinition!) {
         var field = aProperty.GetImplicitField()
         field.Name = "m_\(aProperty.Name)"
         field.Visibility = Visibility.Public
